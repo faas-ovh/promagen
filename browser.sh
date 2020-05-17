ba3e251dcbb4790523aaf53e2499faaf2d07196d
tom <tom@sapletta.com>
2020-05-17 16:03:16 +0200

    update

--- browser.sh
+++
@@ -1,3 +0,0 @@
-explorer "http://localhost:80/docs"
-explorer "http://localhost:80/redoc"
-explorer "http://localhost:80/"
--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ sh nginx-config/install.sh
 #ls /home/nginx
 sh nginx-config/reload.sh
 sh nginx/reload.sh
-sh python/install.sh
-sh python3env/install.sh
-sh gunicorn/install.sh
+#sh python/install.sh
+#sh python3env/install.sh
+#sh gunicorn/install.sh
+sh node/install.sh
+sh src/install.sh
 
