5aab2a51c97bfb226a62237e5605ab2278366cb7
tom <tom@sapletta.com>
2020-05-17 15:32:36 +0200

    update

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ cd src && git pull origin master && cd ..
 cd python && git pull origin master && cd ..
 cd python3env && git pull origin master && cd ..
 cd gunicorn && git pull origin master && cd ..
-#cd nginx && git pull origin master && cd ..
+cd nginx && git pull origin master && cd ..
